1c35e6700403f7769966f3700b6d79efa3e55d37
bninja <andrew@poundpay.com>
2014-03-02 13:07:50 -0800

    semver

--- src/slurp/__init__.py
+++ src/slurp/__init__.py
@@ -16,7 +16,7 @@ from .source import Source, SourceSettings
 from .channel import Channel, ChannelSource, ChannelSettings, ChannelEvent
 from .config import Config
 
-__version__ = '0.6'
+__version__ = '0.6.0'
 
 __all__ = [
     'settings',
