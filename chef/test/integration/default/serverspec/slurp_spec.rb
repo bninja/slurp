ab91c24caec54811e3d1d0b4b5017a0792a201d8
bninja <andrew@poundpay.com>
2014-03-06 20:17:27 -0800

    more chef tests

--- chef/test/integration/default/serverspec/slurp_spec.rb
+++ chef/test/integration/default/serverspec/slurp_spec.rb
@@ -8,14 +8,31 @@ describe command('slurp -v') do
   it { should return_exit_status 0 }
 end
 
-describe command('slurp sources') do
-  it { should return_stdout 'test-access' }
-end
-
 describe command('slurp channels') do
-  it { should return_stdout 'test-access-stats' }
+  it { should return_exit_status 0 }
+  it {
+      should return_stdout <<-EOF
+test-channel
+EOF
+    }
 end
 
-describe command('slurp channel consume /tmp/slurp-test') do
+describe command('slurp sources') do
   it { should return_exit_status 0 }
+  it {
+      should return_stdout <<-EOF
+test-source
+EOF
+    }
+end
+
+describe command('slurp consume -b -t /tmp/test-slurp/test.log') do
+  it { should return_exit_status 0 }
+  it {
+    should return_stdout <<-EOF
+bytes: 1140
+count: 3
+test-channel test-source /tmp/test-slurp/test.log 3 1140 0
+EOF
+  }
 end
