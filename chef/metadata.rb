f0c3c62c5f284ec53eeb4f206c918066a7968735
bninja <andrew@poundpay.com>
2014-03-12 17:17:09 -0700

    stable cookbook

--- chef/metadata.rb
+++ chef/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'egon@gb.com'
 license          'MIT'
 description      'Installs/Configures slurp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 depends          'balanced-apt'
